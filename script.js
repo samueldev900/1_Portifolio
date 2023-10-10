8164f9bc99e4e2be2307b56274d3225cf3fb0253
samueldev900 <samueloliveira900@gmail.com>
2023-10-10 19:40:30 -0300

    Adicionado o menu lateral

---
+++ script.js
@@ -0,0 +1,16 @@
+function btnMenu(){
+
+    var b = document.getElementById('imenu-lateral');
+    b.style.left = "0%"
+    b.style.transition = '0.5s'
+
+}
+
+function btnFechar(){
+
+    var b = document.getElementById('imenu-lateral');
+    b.style.left = "120%"
+    b.style.transition = '0.5s'
+
+}
+
