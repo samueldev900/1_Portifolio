02a0bc9c558296ed194a2714f90688f94858c90c
samueldev900 <samueloliveira900@gmail.com>
2023-10-11 11:21:22 -0300

    Correção de falhas no menu lateral e posicionamento dos textos.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function btnMenu(){
 
     var b = document.getElementById('imenu-lateral');
+    var a = document.getElementById('main')
+    a.style.display = 'none'
     b.style.left = "0%"
-    b.style.transition = '0.5s'
-
+    b.style.transition = '0.3s'
 }
 
 function btnFechar(){
-
     var b = document.getElementById('imenu-lateral');
     b.style.left = "120%"
-    b.style.transition = '0.5s'
+    b.style.transition = '0.1s'
+    var a = document.getElementById('main')
+    a.style.display = 'block'
 
 }
 
